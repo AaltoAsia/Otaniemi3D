288d77f8b696189607c8165d5f5668fa23715445
Antti Mäkipää <makipaa.antti@gmail.com>
2014-11-18 19:59:59 +0200

    Changed to use D3js library for handling svg

--- app/scripts/controllers/twodviewcontroller.js
+++ app/scripts/controllers/twodviewcontroller.js
@@ -9,17 +9,12 @@
  */
 angular.module('otaniemi3dApp')
   .controller('twodview', function ($scope) {
-  var snap = Snap('#floorplan-svg');
 
-  Snap.load('/floorplans/Basement.svg', function (f) {
-    //Doesn't work yet because it doesn't override CSS.
-    var roomToColor = f.select('#shape84-176');
-    roomToColor.attr({
-      stroke: 'green'
-    });
-    snap.append(f);
-  });
-    
+    d3.xml('/floorplans/Basement.svg', "image/svg+xml", function(xml) {
+      console.log(xml.documentElement);
+      d3.select('.floorplan').node().appendChild(xml.documentElement);
+      d3.selectAll('rect').style('fill', 'green');
+    }); 
 
   });
 
