7b865a23ba2c85c5c0bf7e3d566223071ad148a4
Autio Ilmari <ilmari.autio@aalto.fi>
2014-11-25 18:21:06 +0200

    Room colors now change on mouseover

--- app/scripts/controllers/twodviewcontroller.js
+++ app/scripts/controllers/twodviewcontroller.js
@@ -9,14 +9,15 @@
  */
 angular.module('otaniemi3dApp')
   .controller('twodview', function ($scope) {
-
+  
     d3.xml('/floorplans/Basement.svg', "image/svg+xml", function(xml) {
       
       var svgDocument = d3.select(xml.documentElement);
-      svgDocument.attr('width', '500');
-      svgDocument.attr('height', '500');
+      //svgDocument.attr('width', '500');
+      //svgDocument.attr('height', '500');
       
-      d3.select('.floorplan').node().appendChild(xml.documentElement);
+      var svg = d3.select('.floorplan').node().appendChild(xml.documentElement);
+      d3.select('svg').attr('pointer-events', 'all');
       
       d3.selectAll('rect')
         .on('mouseover', function(){
@@ -33,7 +34,16 @@ angular.module('otaniemi3dApp')
         .on('mouseout', function(){
           d3.select(this).style('fill', null);
         });
-    }); 
-
+      
+      var zoomListener = d3.behavior.zoom()
+        .scaleExtent([1, 10])
+        .on('zoom', zoomHandler);
+  
+      function zoomHandler() {
+        d3.select(this).attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+      }
+      
+      d3.select('g').call(zoomListener);
+    });
   });
 
