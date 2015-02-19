1ea044961c1da289d8c0fcb0f9cbbe8177c0e608
Rolle Saarinen <jrsaarin@niksula.hut.fi>
2015-02-20 01:12:53 +0200

    Remove small margin on mobile devices and change background colour to blue.

--- app/scripts/controllers/homecontroller.js
+++ app/scripts/controllers/homecontroller.js
@@ -9,34 +9,30 @@
  */
 angular.module('otaniemi3dApp')
     .controller('home', function ($scope) {
-        $scope.interval = 3000;
+        $scope.interval = 0;
         var slides = $scope.slides = [];
         slides.push({
-            image: 'images/carouselBackground.png',
             caption: 'Otaniemi3D - enhance building efficiency',
             description: 'With Otaniemi3d you can access building sensor values in three different ways.',
             link: 'support',
             linktext: 'Support'
         });
         slides.push({
-            image: 'images/carouselBackground.png',
             caption: '1d View',
             description: 'In 1d view you can see each value recorded by each sensor of k1 building in sortable 2-dimension grid.',
             link: '1dview',
             linktext: '1d View'
         });
         slides.push({
-            image: 'images/carouselBackground.png',
             caption: '2d View',
-            description: '',
+            description: 'Some text to emphasize how awesome our 2dview is',
             link: '2dview',
             linktext: '2d View'
 
         });
         slides.push({
-            image: 'images/carouselBackground.png',
             caption: '3d View',
-            description: '',
+            description: 'Some text to emphasize how awesome our 3dview is and what it does',
             link: '3dview',
             linktext: '3d View'
 
