ee09fe27a0ad2e0d998f3b7ba13b6b6e51e1abfd
Rolle Saarinen <jrsaarin@niksula.hut.fi>
2015-02-21 03:21:05 +0200

    X3d model to fullscreen and change home screen colour

--- app/scripts/controllers/homecontroller.js
+++ app/scripts/controllers/homecontroller.js
@@ -12,31 +12,32 @@ angular.module('otaniemi3dApp')
         $scope.interval = 0;
         var slides = $scope.slides = [];
         slides.push({
-            caption: 'Otaniemi3D - enhance building efficiency',
-            description: 'With Otaniemi3D you can access building sensor values in three different ways.',
-            link: 'support',
-            linktext: 'Support'
-        });
-        slides.push({
-            caption: '1D View',
-            description: 'In 1D View you can see each value recorded by each sensor of k1 building in sortable 2-dimension grid.',
-            link: '1dview',
-            linktext: '1D View'
-        });
-        slides.push({
-            caption: '2D View',
-            description: 'Some text to emphasize how awesome our 2D View is',
-            link: '2dview',
-            linktext: '2D View'
+                caption: 'Otaniemi3D - enhance building efficiency',
+                description: 'With Otaniemi3D you can visualize the sensor values of k1-building of Otaniemi',
+                link: 'support',
+                linktext: 'Support'
+            },
+            {
+                caption: '1D View',
+                description: 'In 1D View you can see each value recorded by each sensor of k1-building in sortable 2-dimension grid',
+                link: '1dview',
+                linktext: '1D View'
+            },
+            {
+                caption: '2D View',
+                description: '2d view provides floor plans of k1-building with heat maps of each sensor values',
+                link: '2dview',
+                linktext: '2D View'
 
-        });
-        slides.push({
-            caption: '3D View',
-            description: 'Some text to emphasize how awesome our 3D View is and what it does',
-            link: '3dview',
-            linktext: '3D View'
+            },
+            {
+                caption: '3D View',
+                description: 'In 3d view you can see the 3d model of the k1-building. From that view you can search' +
+                    ' for rooms of k1 and see the panoramic views of those',
+                link: '3dview',
+                linktext: '3D View'
 
-        });
+            });
 
     });
 
