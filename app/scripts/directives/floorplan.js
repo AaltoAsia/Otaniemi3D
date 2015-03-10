6e3fbedd9e1c5f62459ebe0b460ed14721217cd2
Antti Mäkipää <antti.makipaa@aalto.fi>
2015-03-10 17:34:22 +0200

    Add an example legend piece.

--- app/scripts/directives/floorplan.js
+++ app/scripts/directives/floorplan.js
@@ -486,24 +486,83 @@ angular.module('otaniemi3dApp')
         }
         
         function fillLegend() {
-          var legend = d3.select('#heatmap-legend');
-          legend
-            .append('rect')
-              .attr('fill', 'url(#tempGradient)')
-              .attr("x",100)
-              .attr("y",100)
-              .attr("width",40)
-              .attr("height",100);
-          legend
-            .append('g')
-              .append('defs')
-                .append('tempGradient')
-                  .attr('id', 'tempGradient')
-                  .attr("x1","0%")
-                  .attr("x2","0%")
-                  .attr("y1","0%")
-                  .attr("y2","100%");
+          var svgWidth = 40,
+              svgHeight = 200,
+              x1 = 0,
+              barWidth = svgWidth,
+              y1 = 0,
+              barHeight = svgHeight,
+              numberHues = 35;
+
+          var idGradient = "legendGradient";
+
+          var svgForLegendStuff = d3.select("#theBar").append("svg")
+                                      .attr("width", svgWidth)
+                                      .attr("height", svgHeight);
+
+          //create the empty gradient that we're going to populate later
+          svgForLegendStuff.append("g")
+                              .append("defs")
+                              .append("linearGradient")
+                                  .attr("id",idGradient)
+                                  .attr("x1","0%")
+                                  .attr("x2","0%")
+                                  .attr("y1","0%")
+                                  .attr("y2","100%"); // x1=0, x2=100%, y1=y2 results in a horizontal gradient
+                                                    // it would have been vertical if x1=x2, y1=0, y2=100%
+                                                    // See 
+                                                    //      http://www.w3.org/TR/SVG/pservers.html#LinearGradients
+                                                    // for more details and fancier things you can do
+          //create the bar for the legend to go into
+          // the "fill" attribute hooks the gradient up to this rect
+          svgForLegendStuff.append("rect")
+                              .attr("fill","url(#" + idGradient + ")")
+                              .attr("x",x1)
+                              .attr("y",y1)
+                              .attr("width",barWidth)
+                              .attr("height",barHeight)
+                              .attr("rx",20)  //rounded corners, of course!
+                              .attr("ry",20);
+
+          //we go from a somewhat transparent blue/green (hue = 160º, opacity = 0.3) to a fully opaque reddish (hue = 0º, opacity = 1)
+          var hueStart = 160, hueEnd = 0;
+          var opacityStart = 0.3, opacityEnd = 1.0;
+          var theHue, rgbString, opacity,p;
+
+          var deltaPercent = 1/(numberHues-1);
+          var deltaHue = (hueEnd - hueStart)/(numberHues - 1);
+          var deltaOpacity = (opacityEnd - opacityStart)/(numberHues - 1);
+
+          //kind of out of order, but set up the data here 
+          var theData = [];
+          for (var i=0;i < numberHues;i++) {
+              theHue = hueStart + deltaHue*i;
+              //the second parameter, set to 1 here, is the saturation
+              // the third parameter is "lightness"    
+              rgbString = d3.hsl(theHue,1,0.6).toString();
+              opacity = opacityStart + deltaOpacity*i;
+              p = 0 + deltaPercent*i;
+              //onsole.log("i, values: " + i + ", " + rgbString + ", " + opacity + ", " + p);
+              theData.push({"rgb":rgbString, "opacity":opacity, "percent":p});       
+          }
+
+          //now the d3 magic (imo) ...
+          var stops = d3.select('#' + idGradient).selectAll('stop')
+                              .data(theData);
+
+              stops.enter().append('stop');
+              stops.attr('offset',function(d) {
+                                      return d.percent;
+                          })
+                          .attr('stop-color',function(d) {
+                                      return d.rgb;
+                          })
+                          .attr('stop-opacity',function(d) {
+                                      return d.opacity;
+                          });
         }
+        
+        fillLegend();
 
         /*
         * Watch for changes in twodviewcontroller's $scope.floorplan and
