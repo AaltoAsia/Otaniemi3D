0edb37da75ceadf3a801f90babee13bbcdd0d792
Antti Mäkipää <antti.makipaa@aalto.fi>
2015-03-30 14:21:56 +0300

    Search button function for 2d-view.

--- app/scripts/controllers/twodviewcontroller.js
+++ app/scripts/controllers/twodviewcontroller.js
@@ -9,176 +9,189 @@
  */
 angular.module('otaniemi3dApp')
     .controller('twodview', function ($scope, Datahandler, Floorplans, Rooms, twodservice, $rootScope, $modal) {
-    var loaded = false;
-
-    $scope.panoramaViewer = function() {
-        $scope.pano = true;
-        if(loaded === false){
-            embedpano({xml:'panorama/Room_' + $scope.room +'.xml', id:'pano_obj', target:'pano', html5:'only', passQueryParameters:true});
-            loaded = true;
-        }
-        else{
-            var xmlpath = 'Room_' + $scope.room +'.xml';
-            document.getElementById('pano_obj').call('loadpano('+ xmlpath +');');
-        }
-    };
-    $scope.stopPanorama = function(){
-        $scope.pano = false;
-    };
-
-        var floorplanClass = 'floorplan';
-        var floorplanFullscreenClass = 'floorplan-fullscreen';
-                
-        $scope.sensorData = null;
-        $scope.floorplanClass = floorplanClass;
-        $scope.rooms = Rooms;
-        $scope.searchString = '';
-        $scope.highlightedRoom = null;
-        $scope.roomValueType = 'Temperature';
-        $scope.floors = Floorplans.floors.length;
-        $scope.selectedRoom = null;
-        $scope.timeFrame = '';
-        $scope.room = null; // Room which panoramic button was clicked.
-        $scope.selectedPlan = null;
-        $scope.timeFrame = 'Latest';
-
-        $scope.searchContainer = ''; //This is used to set correct top margin for search container
-
-        $scope.svgSupport = Modernizr.svg;
-        $scope.pano = false;
-
-        /* These are ng-class definitions for buttons found in 2dview*/
-        $scope.buttonClass = 'glyphicon glyphicon-resize-full';
-        $scope.nextButtonClass = 'glyphicon glyphicon-arrow-right';
-        $scope.previousButtonClass = 'glyphicon glyphicon-arrow-left';
-
-        //Select default floorplan which is defined in Floorplans service
-        $scope.planNumber = 0;
-        for ($scope.planNumber; $scope.planNumber < Floorplans.floors.length; $scope.planNumber++) {
-            if (Floorplans.floors[$scope.planNumber].isSelected) {
-                $scope.selectedPlan = Floorplans.floors[$scope.planNumber];
-                break;
-            }
-        }
-
-        // Toggle fullscreen button. It broadcasts to rootscope to change the view to fullscreen
-        // which in turn hides the footer and header. Also it changes the fullscreen button glyphicon
-        $scope.toggleFullscreen = function(){
-            $rootScope.fullscreen = !$rootScope.fullscreen;
-            if ($scope.floorplanClass === floorplanClass) {
-                $scope.floorplanClass = floorplanFullscreenClass;
-                $scope.searchContainer = 'search-container-full';
-                $scope.buttonClass = ' glyphicon glyphicon-resize-small';
-            }
-            else {
-                $scope.floorplanClass = floorplanClass;
-                $scope.searchContainer = '';
-                $scope.buttonClass = 'glyphicon glyphicon-resize-full';
-            }
-
-        };
-
-        /*
-         * Fetch sensor data from the server.
-         */
-        Datahandler.fetchData().then(
-            function(data) {
-                $scope.sensorData = data;
-            },
-            function() {
-                console.log('Error: Failed to fetch sensor data');
-            }
-        );
-        /*
-         * Change current floorplan to the previous of net floorplan
-         * direction is either 1 if the user pressed next button or -1
-         * if the user pressed previous button
-         */
-        $scope.selectPlan = function (direction) {
-
-          if (direction === 1) {
-            Floorplans.floors[$scope.planNumber].isSelected = false;
-            Floorplans.floors[$scope.planNumber+1].isSelected = true;
-            $scope.selectedPlan = Floorplans.floors[$scope.planNumber+1];
-            $scope.planNumber++;
-          }
-          if (direction === -1) {
-            Floorplans.floors[$scope.planNumber].isSelected = false;
-            Floorplans.floors[$scope.planNumber-1].isSelected = true;
-            $scope.selectedPlan = Floorplans.floors[$scope.planNumber-1];
-            $scope.planNumber--;
-          }
-        };
-
-
-        $scope.highlightRoom = function(item) {
-
-	      if ($scope.highlightedRoom !== null) {
-            clearInterval($scope.highlightedRoom.pulse);
-          }
-          
-          $scope.highlightedRoom = item;
-          $scope.planNumber = $scope.highlightedRoom.floor;
-    	};
   
-  	    $scope.onSelect = function($item) {
-          $scope.highlightRoom($item);
-        };
+  var loaded = false;
 
-        /*
-        / Refresh the room colours according to sensor that is chosen.
-        / For example if the user changes from temperature heatmap to co2 heatmap
-        / this function will colour the floorplans according to values measured by
-        / co2 sensors.
-        */
-        $scope.refreshRoomColor = function(type) {
-          for (var j = 0; j < Rooms.list.length; j++) {
-            var room = Rooms.list[j];
+  $scope.panoramaViewer = function() {
+      $scope.pano = true;
+      if(loaded === false){
+          embedpano({xml:'panorama/Room_' + $scope.room +'.xml', id:'pano_obj', target:'pano', html5:'only', passQueryParameters:true});
+          loaded = true;
+      }
+      else{
+          var xmlpath = 'Room_' + $scope.room +'.xml';
+          document.getElementById('pano_obj').call('loadpano('+ xmlpath +');');
+      }
+  };
+  $scope.stopPanorama = function(){
+      $scope.pano = false;
+  };
 
-            // Colour the room white, in case the room doesn't have any values for that particular sensor
-            //
-            d3.select(room.node).style('fill', 'rgb(255, 255, 255)');
+  var floorplanClass = 'floorplan';
+  var floorplanFullscreenClass = 'floorplan-fullscreen';
 
-            // Loop through sensors and check the value of the sensor that matches the parameter given
-            //
-            for (var i = 0; i < room.sensors.length; i++) {
-              if (room.sensors[i].type.toLowerCase() === type.toLowerCase() 
-                  || (room.sensors[i].type.toLowerCase() === 'pir' && type.toLowerCase() === 'occupancy')) {
-                var color = twodservice.getColor(room.sensors[i].type, room.sensors[i].value);
-                d3.select(room.node)
-                  .style('fill', color.rgb)
-                  .style('fill-opacity', color.opacity);
-              }
-            }
-          }
-        };
+  $scope.sensorData = null;
+  $scope.floorplanClass = floorplanClass;
+  $scope.rooms = Rooms;
+  $scope.searchString = '';
+  $scope.highlightedRoom = null;
+  $scope.roomValueType = 'Temperature';
+  $scope.floors = Floorplans.floors.length;
+  $scope.selectedRoom = null;
+  $scope.timeFrame = '';
+  $scope.room = null; // Room which panoramic button was clicked.
+  $scope.selectedPlan = null;
+  $scope.timeFrame = 'Latest';
 
-        $scope.changeColour = function(type) {
-            $scope.roomValueType = type;
-            $scope.refreshRoomColor(type);
-        };
+  $scope.searchContainer = ''; //This is used to set correct top margin for search container
 
-        $scope.selectTimeFrame = function(timeFrame) {
-            var time = timeFrame;
-          
-            if (time) {
-              $scope.timeFrame = time;
-            } else {
-              $scope.timeFrame = 'Latest';
-            }
-          
-            Datahandler.fetchData(time).then(
-              function(data) {
-                  $scope.sensorData = data;
-              },
-              function() {
-                  console.log('Error: Failed to fetch sensor data');
-              }
-          );
-        };
-        
+  $scope.svgSupport = Modernizr.svg;
+  $scope.pano = false;
 
-        
+  /* These are ng-class definitions for buttons found in 2dview*/
+  $scope.buttonClass = 'glyphicon glyphicon-resize-full';
+  $scope.nextButtonClass = 'glyphicon glyphicon-arrow-right';
+  $scope.previousButtonClass = 'glyphicon glyphicon-arrow-left';
+
+  //Select default floorplan which is defined in Floorplans service
+  $scope.planNumber = 0;
+  for ($scope.planNumber; $scope.planNumber < Floorplans.floors.length; $scope.planNumber++) {
+      if (Floorplans.floors[$scope.planNumber].isSelected) {
+          $scope.selectedPlan = Floorplans.floors[$scope.planNumber];
+          break;
+      }
+  }
+
+  // Toggle fullscreen button. It broadcasts to rootscope to change the view to fullscreen
+  // which in turn hides the footer and header. Also it changes the fullscreen button glyphicon
+  $scope.toggleFullscreen = function(){
+      $rootScope.fullscreen = !$rootScope.fullscreen;
+      if ($scope.floorplanClass === floorplanClass) {
+          $scope.floorplanClass = floorplanFullscreenClass;
+          $scope.searchContainer = 'search-container-full';
+          $scope.buttonClass = ' glyphicon glyphicon-resize-small';
+      }
+      else {
+          $scope.floorplanClass = floorplanClass;
+          $scope.searchContainer = '';
+          $scope.buttonClass = 'glyphicon glyphicon-resize-full';
+      }
+
+  };
+
+  /*
+   * Fetch sensor data from the server.
+   */
+  Datahandler.fetchData().then(
+      function(data) {
+          $scope.sensorData = data;
+      },
+      function() {
+          console.log('Error: Failed to fetch sensor data');
+      }
+  );
+  /*
+   * Change current floorplan to the previous of net floorplan
+   * direction is either 1 if the user pressed next button or -1
+   * if the user pressed previous button
+   */
+  $scope.selectPlan = function (direction) {
+
+    if (direction === 1) {
+      Floorplans.floors[$scope.planNumber].isSelected = false;
+      Floorplans.floors[$scope.planNumber+1].isSelected = true;
+      $scope.selectedPlan = Floorplans.floors[$scope.planNumber+1];
+      $scope.planNumber++;
+    }
+    if (direction === -1) {
+      Floorplans.floors[$scope.planNumber].isSelected = false;
+      Floorplans.floors[$scope.planNumber-1].isSelected = true;
+      $scope.selectedPlan = Floorplans.floors[$scope.planNumber-1];
+      $scope.planNumber--;
+    }
+  };
+
+
+  $scope.highlightRoom = function(item) {
+
+    if ($scope.highlightedRoom !== null) {
+      clearInterval($scope.highlightedRoom.pulse);
+    }
+
+    $scope.highlightedRoom = item;
+    $scope.planNumber = $scope.highlightedRoom.floor;
+  };
+
+  $scope.onSelect = function($item) {
+    $scope.highlightRoom($item);
+  };
+
+  $scope.onSearch = function(searchString) {
+    var selected;
+    for (var i = 0; i < Rooms.list.length; i++) {
+      var room = Rooms.list[i];
+      if (room.name.toLowerCase() === searchString.toLowerCase()) {
+        selected = room;
+        break;
+      }
+    };
+    $scope.highlightRoom(selected);
+  };
+
+  /*
+  / Refresh the room colours according to sensor that is chosen.
+  / For example if the user changes from temperature heatmap to co2 heatmap
+  / this function will colour the floorplans according to values measured by
+  / co2 sensors.
+  */
+  $scope.refreshRoomColor = function(type) {
+    for (var j = 0; j < Rooms.list.length; j++) {
+      var room = Rooms.list[j];
+
+      // Colour the room white, in case the room doesn't have any values for that particular sensor
+      //
+      d3.select(room.node).style('fill', 'rgb(255, 255, 255)');
+
+      // Loop through sensors and check the value of the sensor that matches the parameter given
+      //
+      for (var i = 0; i < room.sensors.length; i++) {
+        if (room.sensors[i].type.toLowerCase() === type.toLowerCase() 
+            || (room.sensors[i].type.toLowerCase() === 'pir' && type.toLowerCase() === 'occupancy')) {
+          var color = twodservice.getColor(room.sensors[i].type, room.sensors[i].value);
+          d3.select(room.node)
+            .style('fill', color.rgb)
+            .style('fill-opacity', color.opacity);
+        }
+      }
+    }
+  };
+
+  $scope.changeColour = function(type) {
+      $scope.roomValueType = type;
+      $scope.refreshRoomColor(type);
+  };
+
+  $scope.selectTimeFrame = function(timeFrame) {
+      var time = timeFrame;
+
+      if (time) {
+        $scope.timeFrame = time;
+      } else {
+        $scope.timeFrame = 'Latest';
+      }
+
+      Datahandler.fetchData(time).then(
+        function(data) {
+            $scope.sensorData = data;
+        },
+        function() {
+            console.log('Error: Failed to fetch sensor data');
+        }
+    );
+  };
+
+
+
    /*Create a new modal pass timeframe and roomValueType variables into it
       Also parse the return values to aforementioned variables*/
   $scope.open = function () {
@@ -195,7 +208,7 @@ angular.module('otaniemi3dApp')
         }
       }
     });
-    
+
     modalInstance.result.then(function () {
       if (arguments[0][1] !== $scope.timeFrame) {
         $scope.timeFrame = arguments[0][1];
@@ -207,5 +220,5 @@ angular.module('otaniemi3dApp')
         $scope.refreshRoomColor($scope.roomValueType);
       }
     });
-    };
-    });
+  };
+});
