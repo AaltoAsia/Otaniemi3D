08c6f450d02983b4c2a9d5cd02f758cd712049f1
Ghesus <eero.helske@gmail.com>
2015-04-10 05:44:02 +0300

    add new background pictures

--- app/scripts/controllers/homecontroller.js
+++ app/scripts/controllers/homecontroller.js
@@ -16,19 +16,22 @@ angular.module('otaniemi3dApp')
                 description: 'With Otaniemi3D you can visualize the sensor values of k1-building of Otaniemi',
                 link: 'support',
                 linktext: 'Support',
-                img: '/images/3dview.png'
+                img: 'images/home.png'
             },
             {
                 caption: '1D View',
                 description: 'In 1D View you can see each value recorded by each sensor of k1-building in sortable 2-dimension grid',
                 link: '1dview',
-                linktext: '1D View'
+                linktext: '1D View',
+                img: 'images/1d.png'
             },
             {
                 caption: '2D View',
                 description: '2d view provides floor plans of k1-building with heat maps of each sensor values',
                 link: '2dview',
-                linktext: '2D View'
+                linktext: '2D View',
+                img: 'images/2d.png'
+
 
             },
             {
@@ -36,7 +39,8 @@ angular.module('otaniemi3dApp')
                 description: 'In 3d view you can see the 3d model of the k1-building. From that view you can search' +
                     ' for rooms of k1 and see the panoramic views of those',
                 link: '3dview',
-                linktext: '3D View'
+                linktext: '3D View',
+                img: 'images/3d.png'
 
             });
 
