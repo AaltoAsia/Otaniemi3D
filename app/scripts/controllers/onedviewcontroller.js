c0e1bf94049688003389dab21d4e7804985177d3
Ilmari Autio <ilmari.autio@gmail.com>
2015-04-12 19:37:22 +0300

    Add menu for selecting time frame in the 1D view

--- app/scripts/controllers/onedviewcontroller.js
+++ app/scripts/controllers/onedviewcontroller.js
@@ -11,6 +11,27 @@ angular.module('otaniemi3dApp')
 
   .controller('onedview', function ($scope, $location, Datahandler) {
 
+    $scope.timeFrame = 'Latest';
+
+    function selectTimeFrame(timeFrame) {
+      var time = timeFrame;
+      
+      if (time) {
+        $scope.timeFrame = time;
+      } else {
+        $scope.timeFrame = 'Latest';
+      }
+      
+      Datahandler.fetchData(time).then(
+        function(data) {
+            $scope.gridOptions.data = data;
+        },
+        function() {
+            console.log('Error: Failed to fetch sensor data');
+        }
+      );
+    }
+
     var fetchDataPromise = Datahandler.fetchData();
     fetchDataPromise
       .then(function (data) {
@@ -18,30 +39,80 @@ angular.module('otaniemi3dApp')
         }, function (reason) {    // something gone wrong
         $scope.gridOptions.data = reason;
       });
-       $scope.gridOptions = {
-        enableFiltering: true,
-        columnDefs: [
-          {
-            field: 'room',
-            filter: {
-              condition:  function(searchTerm, cellValue) {
-                var strippedValue = cellValue.toLowerCase();
-                return strippedValue.indexOf(searchTerm.toLowerCase()) >= 0;
-                }
-              }
-            },
-          {
-            field: 'type',
-            filter: {
-              condition:  function(searchTerm, cellValue) {
-                var strippedValue = cellValue.toLowerCase();
-                return strippedValue.indexOf(searchTerm.toLowerCase()) >= 0;
-                }
-              }
-            },
-          {
-            field: 'value'
+
+    $scope.gridOptions = {
+      enableFiltering: true,
+      enableGridMenu: true,
+      gridMenuShowHideColumns: false,
+      gridMenuCustomItems: [
+        {
+          title: 'Latest',
+          action: function() {
+            selectTimeFrame('Latest');
+          },
+          active: function () {
+            return $scope.timeFrame === 'Latest';
           }
-        ]
-      };
+        },
+        {
+          title: 'Day',
+          action: function() {
+            selectTimeFrame('Day');
+          },
+          active: function () {
+            return $scope.timeFrame === 'Day';
+          }
+        },
+        {
+          title: 'Week',
+          action: function() {
+            selectTimeFrame('Week');
+          },
+          active: function () {
+            return $scope.timeFrame === 'Week';
+          }
+        },
+        {
+          title: 'Month',
+          action: function() {
+            selectTimeFrame('Month');
+          },
+          active: function () {
+            return $scope.timeFrame === 'Month';
+          }
+        },
+        {
+          title: 'Year',
+          action: function() {
+            selectTimeFrame('Year');
+          },
+          active: function () {
+            return $scope.timeFrame === 'Year';
+          }
+        }
+      ],
+      columnDefs: [
+        {
+          field: 'room',
+          filter: {
+            condition:  function(searchTerm, cellValue) {
+              var strippedValue = cellValue.toLowerCase();
+              return strippedValue.indexOf(searchTerm.toLowerCase()) >= 0;
+              }
+            }
+          },
+        {
+          field: 'type',
+          filter: {
+            condition:  function(searchTerm, cellValue) {
+              var strippedValue = cellValue.toLowerCase();
+              return strippedValue.indexOf(searchTerm.toLowerCase()) >= 0;
+              }
+            }
+          },
+        {
+          field: 'value'
+        }
+      ]
+    };
   });
