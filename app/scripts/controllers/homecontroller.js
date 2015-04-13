da550adaf9efc23b463446064f26f45a023d2269
Nils Haglund <nils.haglund@aalto.fi>
2015-04-14 01:02:07 +0300

    add Rolle's foggy images, i.e. make the home view background images better

--- app/scripts/controllers/homecontroller.js
+++ app/scripts/controllers/homecontroller.js
@@ -17,31 +17,31 @@ angular.module('otaniemi3dApp')
                 description: 'With Otaniemi3D you can visualize the sensor values of k1-building of Otaniemi',
                 link: 'support',
                 linktext: 'Support',
-                img: 'images/home2.png'
+                img: 'images/home.jpg'
             },
             {
                 caption: '1D View',
-                description: 'In 1D View you can see each value recorded by each sensor of k1-building in sortable 2-dimension grid',
+                description: 'In 1D View you can see values recorded by sensors of the K1-building in sortable 2-dimension grid',
                 link: '1dview',
                 linktext: '1D View',
-                img: 'images/1d.png'
+                img: 'images/1d.jpg'
             },
             {
                 caption: '2D View',
-                description: '2d view provides floor plans of k1-building with heat maps of each sensor values',
+                description: '2D view displays floor plans of K1-building with heat maps for different sensor values',
                 link: '2dview',
                 linktext: '2D View',
-                img: 'images/2d.png'
+                img: 'images/2d.jpg'
 
 
             },
             {
                 caption: '3D View',
-                description: 'In 3d view you can see the 3d model of the k1-building. From that view you can search' +
-                    ' for rooms of k1 and see the panoramic views of those',
+                description: 'In 3D view you can see the 3D model of the K1-building. You can search' +
+                    ' for rooms of K1 and look at panoramic pictures',
                 link: '3dview',
                 linktext: '3D View',
-                img: 'images/3d.png'
+                img: 'images/3d.jpg'
 
             });
 
