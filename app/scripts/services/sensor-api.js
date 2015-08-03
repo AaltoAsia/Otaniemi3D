b59c539fa7c543f5cc831ff0971334054409f7ea
Ilmari Autio <ilmari.autio@aalto.fi>
2015-08-03 16:00:09 +0300

    [WIP] Fix XML creation on firefox

--- app/scripts/services/sensor-api.js
+++ app/scripts/services/sensor-api.js
@@ -8,7 +8,7 @@
  * Service in the otaniemi3dApp.
  */
 angular.module('otaniemi3dApp')
-  .service('sensorApi', function ($http, $q, $interval, dataStorage) {
+  .service('sensorApi', function ($http, $q, $interval, dataStorage, JXON) {
 
     //Store pending http requests to an object
     var pendingRequests = {},
@@ -281,9 +281,13 @@ angular.module('otaniemi3dApp')
       msg.setAttribute('xmlns', 'odf.xsd');
       msg.setAttributeNS(xsi, 'xsi:schemaLocation', 'odf.xsd odf.xsd');
 
-      var requestBody = createXML(request).documentElement;
-
-      msg.appendChild(requestBody);
+      //request.Objects['@xmlns'] = 'odf.xsd';
+      var requestBody = JXON.createXML(request).documentElement;
+      //console.log(JXON.createXML(request));
+      console.log(requestBody);
+      console.log(new XMLSerializer().serializeToString(requestBody));
+      msg.appendJXON(requestBody);
+      console.log(new XMLSerializer().serializeToString(msg));
       methodElem.appendChild(msg);
       xml.appendChild(methodElem);
 
@@ -318,50 +322,6 @@ angular.module('otaniemi3dApp')
       }
 
       return found;
-    }
-
-    /*
-     * JXON Snippet #5 - Mozilla Developer Network
-     * https://developer.mozilla.org/en-US/docs/JXON
-     */
-    function createXML (oObjTree) {
-      function loadObjTree (oParentEl, oParentObj) {
-        var vValue, oChild;
-        if (oParentObj.constructor === String || oParentObj.constructor === Number || oParentObj.constructor === Boolean) {
-          oParentEl.appendChild(oNewDoc.createTextNode(oParentObj.toString())); /* verbosity level is 0 or 1 */
-          if (oParentObj === oParentObj.valueOf()) { return; }
-        } else if (oParentObj.constructor === Date) {
-          oParentEl.appendChild(oNewDoc.createTextNode(oParentObj.toGMTString()));
-        }
-        for (var sName in oParentObj) {
-          if (isFinite(sName)) { continue; } /* verbosity level is 0 */
-          vValue = oParentObj[sName];
-          if (sName === 'keyValue') {
-            if (vValue !== null && vValue !== true) { oParentEl.appendChild(oNewDoc.createTextNode(vValue.constructor === Date ? vValue.toGMTString() : String(vValue))); }
-          } else if (sName === 'keyAttributes') { /* verbosity level is 3 */
-            for (var sAttrib in vValue) { oParentEl.setAttribute(sAttrib, vValue[sAttrib]); }
-          } else if (sName.charAt(0) === '@') {
-            oParentEl.setAttribute(sName.slice(1), vValue);
-          } else if (vValue.constructor === Array) {
-            for (var nItem = 0; nItem < vValue.length; nItem++) {
-              oChild = oNewDoc.createElement(sName);
-              loadObjTree(oChild, vValue[nItem]);
-              oParentEl.appendChild(oChild);
-            }
-          } else {
-            oChild = oNewDoc.createElement(sName);
-            if (vValue instanceof Object) {
-              loadObjTree(oChild, vValue);
-            } else if (vValue !== null && vValue !== true) {
-              oChild.appendChild(oNewDoc.createTextNode(vValue.toString()));
-            }
-            oParentEl.appendChild(oChild);
-          }
-        }
-      }
-      var oNewDoc = document.implementation.createDocument('', '', null);
-      loadObjTree(oNewDoc, oObjTree);
-      return oNewDoc;
     }
 
   });
